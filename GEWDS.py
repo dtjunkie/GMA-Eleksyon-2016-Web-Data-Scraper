0894f0ae2eb52b492dc5c6f5d2c744d880141bfe
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2022-08-15 22:00:51 +0800

    2nd Revision.
    1) Imported the "os" library
    2) Added variable for the base folder.
    3) Added function for creating directory.
    4) Improved download_data() function so it create directory per administrative level, retrieve election results from all admnistrative levels, converts them to JSON file, and stored in their respective sub-directories.

--- GEWDS.py
+++ GEWDS.py
@@ -1,4 +1,7 @@
 import pandas as pd
+import os
+
+BASE_PATH = "GMAEleksyon2019Data"
 
 def region_dir():
     df = pd.read_json('https://eleksyonconfig2.gmanetwork.com/gno/microsites/eleksyon2019/results/ref/NO/geolocation_REGION.json')
@@ -41,14 +44,41 @@ def region_dir():
     barangay_dir = dict(zip(df['brgy_name'],df['brgy_code']))
     return barangay_dir
   
+  def create_directory(path):
+    print(f"Creating path:{path}")
+    os.makedirs(path)
+    print("Path Created.")
+    
   def download_data(region,province,citymun):
     region_code = region_dir().get(region)
     province_code = province_dir(region).get(province)
     citymun_code = citymun_dir(region,province).get(citymun)
-    brgy_dir = barangay_dir(region,province,citymun)
-    for barangay in brgy_dir.keys():
-        brgy_code = brgy_dir.get(barangay)
+    
+    reg_path = f"{BASE_PATH}/{region}"
+    if os.path.exists(reg_path) == False:
+        create_directory(reg_path)
+    reg_result = pd.read_json(f'https://eleksyondata2019.gmanews.tv/all_lvgs_results/{region_code}.json')
+    print(f'Getting results from {region}...')
+    reg_result.to_json(f'{reg_path}/{region}.json', orient='records')
+    
+    prov_path = f"{BASE_PATH}/{region}/{province}"
+    if os.path.exists(prov_path) == False:
+        create_directory(prov_path)
+    prov_result = pd.read_json(f'https://eleksyondata2019.gmanews.tv/all_lvgs_results/{region_code}_{province_code}.json')
+    print(f'Getting results from {province}, {region}...')
+    prov_result.to_json(f'{prov_path}/{province}.json', orient='records')
+    
+    citymun_path = f"{BASE_PATH}/{region}/{province}/{citymun}"
+    create_directory(citymun_path)
+    citymun_result = pd.read_json(f'https://eleksyondata2019.gmanews.tv/all_lvgs_results/{region_code}_{province_code}_{citymun_code}.json')
+    print(f'Getting results from {citymun}, {province}, {region}...')
+    citymun_result.to_json(f'{citymun_path}/{citymun}.json', orient='records')
+    
+    for barangay in barangay_dir(region,province,citymun).keys():
+        brgy_code = barangay_dir(region,province,citymun).get(barangay)
+        brgy_path = f"{BASE_PATH}/{region}/{province}/{citymun}/{barangay}"
+        create_directory(brgy_path)
         df = pd.read_json(f'https://eleksyondata2019.gmanews.tv/all_lvgs_results/{region_code}_{province_code}_{citymun_code}_{brgy_code}.json')
         print(f'Getting results from {barangay}, {citymun}, {province}, {region}...')
-        df.to_json(f'{barangay}.json', orient='records')
-    print('Download Completed!')
+        df.to_json(f'{brgy_path}/{barangay}.json', orient='records')
+    print('Download Completed.')
