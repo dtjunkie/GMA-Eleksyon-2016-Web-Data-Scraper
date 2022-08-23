179d8ec6962ce4d435586fbddf30144bf950fdc9
Daniel Joseph T. Castro <90781857+dtjunkie@users.noreply.github.com>
2022-08-23 17:47:52 +0800

    7th Revision
    Improved the main() function in order to retrieve the following data:
    - National election result for the position of senator
    - National election result for party-list representation
    - List of locations per administrative division

--- GEWDS.py
+++ GEWDS.py
@@ -53,14 +53,38 @@ def create_directory(path):
 
 def main():
     BASE_PATH = "GMAEleksyon2019Data"
+    GEOLOC_URL = 'https://eleksyonconfig2.gmanetwork.com/gno/microsites/eleksyon2019/results'
     BASE_URL = 'https://eleksyondata2019.gmanews.tv/all_lvgs_results'
     DOWNLOAD_DELAY = 1
     
+    geoloc_PH = pd.read_json(f'{GEOLOC_URL}/ref/NO/geolocation_REGION.json')
+    senator_PH = pd.read_json('https://eleksyondata2019.gmanews.tv/national/SENATOR_PHILIPPINES.json')
+    partylist_PH = pd.read_json('https://eleksyondata2019.gmanews.tv/national/PARTY_LIST_PHILIPPINES.json')
+    if os.path.exists(BASE_PATH) == False:
+        create_directory(BASE_PATH)
+    print("Getting list of regions from the PHILIPPINES...")
+    sleep(DOWNLOAD_DELAY)
+    geoloc_PH.to_json(f"{BASE_PATH}/PHILIPPINES.json", orient='records')
+    print("Getting senatorial results from the PHILIPPINES...")
+    sleep(DOWNLOAD_DELAY)
+    senator_PH.to_json(f"{BASE_PATH}/SENATOR_PHILIPPINES.json", orient='records')
+    print("Getting party-list results from the PHILIPPINES...")
+    sleep(DOWNLOAD_DELAY)
+    partylist_PH.to_json(f"{BASE_PATH}/PARTY_LIST_PHILIPPINES.json", orient='records')
+    
     for region in region_dir().keys():
         region_code = region_dir().get(region)
+        region_ref = region_code[:-3:-1]
         reg_path = f"{BASE_PATH}/{region}"
         if os.path.exists(reg_path) == False:
             create_directory(reg_path)
+        if os.path.exists(f'{reg_path}/GEOLOC_{region}.json') == False:
+            geoloc_region = pd.read_json(f'{GEOLOC_URL}/ref/{region_ref}/geolocation_{region_code}.json')
+            print(f'Getting list of provinces in {region}...')
+            sleep(DOWNLOAD_DELAY)
+            geoloc_region.to_json(f'{reg_path}/GEOLOC_{region}.json', orient='records')
+        else:
+            print(f'{reg_path}/GEOLOC_{region}.json exists.')
         if os.path.exists(f'{reg_path}/{region}.json') == False:
             reg_result = pd.read_json(f'{BASE_URL}/{region_code}.json')
             print(f'Getting results from {region}...')
@@ -73,8 +97,16 @@ def main():
         for province in province_dir(region).keys():
             province_code = province_dir(region).get(province)
             prov_path = f"{reg_path}/{province}"
+            prov_ref = province_code[:-3:-1]
             if os.path.exists(prov_path) == False:
                 create_directory(prov_path)
+            if os.path.exists(f'{prov_path}/GEOLOC_{province}.json') == False:
+                geoloc_province = pd.read_json(f'{GEOLOC_URL}/ref/{prov_ref}/geolocation_{region_code}_{province_code}.json')
+                print(f'Getting list of cities/municipalities in {province}, {region}...')
+                sleep(DOWNLOAD_DELAY)
+                geoloc_province.to_json(f'{prov_path}/GEOLOC_{province}.json', orient='records')
+            else:
+                print(f'{prov_path}/GEOLOC_{province}.json exists.')
             if os.path.exists(f'{prov_path}/{province}.json') == False:
                 prov_result = pd.read_json(f'{BASE_URL}/{region_code}_{province_code}.json')
                 print(f'Getting results from {province}, {region}...')
@@ -87,8 +119,16 @@ def main():
             for citymun in citymun_dir(region,province).keys():
                 citymun_code = citymun_dir(region,province).get(citymun)
                 citymun_path = f"{prov_path}/{citymun}"
+                citymun_ref = citymun_code[:-3:-1]
                 if os.path.exists(citymun_path) == False:
-                    create_directory(citymun_path)        
+                    create_directory(citymun_path)
+                if os.path.exists(f'{citymun_path}/GEOLOC_{citymun}.json') == False:
+                    geoloc_citymun = pd.read_json(f'{GEOLOC_URL}/ref/{citymun_ref}/geolocation_{region_code}_{province_code}_{citymun_code}.json')
+                    print(f'Getting list of barangays in {citymun}, {province}, {region}...')
+                    sleep(DOWNLOAD_DELAY)
+                    geoloc_citymun.to_json(f'{citymun_path}/GEOLOC_{citymun}.json', orient='records')
+                else:
+                    print(f'{citymun_path}/GEOLOC_{citymun}.json exists.')
                 if os.path.exists(f'{citymun_path}/{citymun}.json') == False:
                     try:
                         citymun_result = pd.read_json(f'{BASE_URL}/{region_code}_{province_code}_{citymun_code}.json')
@@ -106,8 +146,16 @@ def main():
                     brgy_code = barangay_dir(region,province,citymun).get(barangay)
                     barangay = barangay.replace("*","_")
                     brgy_path = f"{citymun_path}/{barangay}"
+                    brgy_ref = brgy_code[:-3:-1]
                     if os.path.exists(brgy_path) == False:
                         create_directory(brgy_path)        
+                    if os.path.exists(f'{brgy_path}/GEOLOC_{barangay}.json') == False:
+                        geoloc_brgy = pd.read_json(f'{GEOLOC_URL}/ref/{brgy_ref}/geolocation_{region_code}_{province_code}_{citymun_code}_{brgy_code}.json')
+                        print(f'Getting list of precincts in {barangay}, {citymun}, {province}, {region}...')
+                        sleep(DOWNLOAD_DELAY)
+                        geoloc_brgy.to_json(f'{brgy_path}/GEOLOC_{barangay}.json', orient='records')
+                    else:
+                        print(f'{brgy_path}/GEOLOC_{barangay}.json exists.')
                     if os.path.exists(f'{brgy_path}/{barangay}.json') == False:
                         try:
                             brgy_result = pd.read_json(f'{BASE_URL}/{region_code}_{province_code}_{citymun_code}_{brgy_code}.json')
